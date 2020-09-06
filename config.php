2de6e08068f289ae6aef5dbb93808bc1956a3002
linhfishCR7 <59117535+linhfishCR7@users.noreply.github.com>
2020-09-06 16:49:43 +0700

    hoàn thành

--- config.php
+++ config.php
@@ -1,17 +1,18 @@
 <?php
 
+	
+	$dbhost = 'locahost';
+	$dbname = 'chat';
+	$dbuser = 'root';
+	$dbpass = '';
 
-    $dbhost = 'localhost';
-    $dbname = 'chat';
-    $dbuser = 'root';
-    $dbpass = '';
-    try{
-        $db = new PDO("mysql:dbhost=$dbhost;dbname=$dbname", "$dbuser", "$dbpass");
 
-    }catch( PDOException $e){
-        echo $e->getMessage();
-    }
-
+	try{
+		$db = new PDO("mysql:dbhost=$dbhost;dbname=$dbname", "$dbuser", "$dbpass");
+	}catch( PDOException $e ){
+		echo $e->getMessage();
+	}
+	
 
 
 ?>
